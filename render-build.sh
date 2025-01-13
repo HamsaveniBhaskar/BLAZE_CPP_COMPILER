b60d770bc9af91b4c4dd6ec866c2bcbb79bbdcce
HamsaveniBhaskar <hamsavenibhaskar1@gmail.com>
2025-01-13 09:29:31 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -6,11 +6,8 @@ apt-get install -y clang
 # Install Node.js dependencies
 npm install
 
-# Install pm2 to manage the server process
-npm install -g pm2
-
 echo "Dependencies installed successfully."
 
-# Run the server with pm2 using maximum instances based on CPU cores
-echo "Starting the server with pm2..."
-pm2 start server.js -i max
+# Run the server
+echo "Starting the server..."
+npm start
